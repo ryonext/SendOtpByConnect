6f602690e6f36816e3e1529139f70e83ed898a9f
ryonext <ryonext.s@gmail.com>
2019-10-16 21:54:28 +0900

    Register OTP into DDB.

--- code/app.rb
+++ code/app.rb
@@ -1,15 +1,32 @@
 require 'json'
 require 'aws-sdk-connect'
+require 'aws-sdk-dynamodb'
 require 'yaml'
 
 def lambda_handler(event:, context:)
+  print(event);
   yaml = YAML.load_file("./connect_info.yaml")
   source_number = yaml["source_number"] 
   dest_number = yaml["destination_number"] 
   instance_id = yaml["instance_id"]
   cf_id = yaml["contact_flow_id"]
 
-  otp = rand(100000..999999).to_s
+  otp = create_otp
+
+  # DDB に OTP を入れる
+  table = Aws::DynamoDB::Table.new("CognitoOtpTable")
+  table.update_item(
+    key: {
+      username: event["userName"]
+    },
+    attribute_updates: {
+      otp: {
+        value: otp,
+        action: "PUT"
+      }
+    }
+  )
+
 
   connect = Aws::Connect::Client.new
   connect.start_outbound_voice_contact(
