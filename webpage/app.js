80736ab5ec6b7a212ce126d42953b83d7d9c1fbd
ryonext <ryonext.s@gmail.com>
2019-10-21 23:18:17 +0900

    Create login function.

--- webpage/app.js
+++ webpage/app.js
@@ -60,3 +60,39 @@ $(document).on("click", "#send-otp", () => {
     }
   });
 });
+
+$("#sign-in").on("click", (e) => {
+  e.preventDefault();
+  var authenticationData = {
+      Username : $("#username").val(),
+      Password : $("#password").val(),
+  };
+  var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+  var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+  var userData = {
+    Username : authenticationData.Username,
+    Pool : userPool
+  };
+  var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function (result) {
+      var accessToken = result.getAccessToken().getJwtToken();
+      
+      /* Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer*/
+      var idToken = result.idToken.jwtToken;
+      var refreshToken = result.refreshToken.token;
+      console.log(result);
+      $("#content").replaceWith(`
+        <p>ID Token</p>
+        <p>${idToken}</p>
+        <p>Access Token</p>
+        <p>${accessToken}</p>
+        <p>Refresh Token</p>
+        <p>${refreshToken}</p>
+      `);
+    },
+    onFailure: function(err) {
+      console.log(err);
+    }
+  });
+});;
